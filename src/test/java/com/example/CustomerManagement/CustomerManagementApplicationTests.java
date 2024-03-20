f888e4f5362fbe65dbb032cede5553f255ad61dd
Wojtek <straszewski.wojciech@gmail.com>
2024-03-20 12:08:00 +0100

    init commit

---
+++ src/test/java/com/example/CustomerManagement/CustomerManagementApplicationTests.java
@@ -0,0 +1,54 @@
+package com.example.CustomerManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	public void shouldReturnListOfCustomersWhenExistsByLastNameTest(){
+
+	}
+	@Test
+	public void shouldReturnEmptyListWhenCustomerNotExistsByLastNameTest(){
+
+	}
+	@Test
+	public void shouldReturnListOfCustomersWhenExistsByCityTest(){
+
+	}
+	@Test
+	public void shouldReturnEmptyListWhenCustomerNotExistsByCityTest(){
+
+	}
+	@Test
+	public void shouldAddCustomerWhenNotExistsTest(){
+
+	}
+	@Test
+	public void shouldDeleteCustomerWhenExistsTest() {
+
+	}
+	@Test
+	public void shouldReturn404OnDeleteWhenCustomerNotExistsTest(){
+
+	}
+	@Test
+	public void shouldUpdateCustomerWhenExistsTest(){
+
+	}
+	@Test
+	public void shouldReturn404OnUpdateWhenCustomerNotExistsTest(){
+
+	}
+	@Test
+	public void shouldReturnListOfCustomersWhenQueryForNextOverviewTest(){
+
+	}
+}
+
