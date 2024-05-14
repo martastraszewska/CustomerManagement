d99b22b3fa63c2ce10b1b639213138f44c43c25e
martastraszewska <marta.starszewska2@gmail.com>
2024-05-14 12:17:28 +0200

    add frontend package to project

--- src/main/frontend/src/index.tsx
+++ src/main/frontend/src/index.tsx
@@ -23,17 +23,3 @@ root.render(
    <App />
   </React.StrictMode>
 );
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
